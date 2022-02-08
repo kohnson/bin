821750cb80238dd732dbd36bfb93bc3c107ad27a
kohnson <scp100j@gmail.com>
2022-02-08 14:22:18 -0500

    Fixed naming issue, added git status before and after

--- dots.ps1
+++ dots.ps1
@@ -7,28 +7,34 @@ Function cpask {
 		[Parameter (Mandatory = $true)] $n  # Name
 	)
 	
-	If ($(Read-Host -Prompt "Copy $n? [Y/n]") -ne "n") {Copy-Item -Path $i -Destination $d}
+	If ($(Read-Host -Prompt "Copy $n`? [Y/n]") -ne "n") {
+		Copy-Item -Path $i -Destination $d 
+		$OpDone = $true
+	}
 }
 $DotDir = "~/dotfiles/"
 Set-Location $DotDir
+git status;""
 
 # PowerShell stuff
 cpask $(Get-PoshContext | Select-String ".omp.json") "./common/pwsh/" "Oh-My-Posh prompt"
 cpask $PROFILE "./common/pwsh/" "PowerShell profile"
 
 # ConEmu
-cpask "$HOME/scoop/persist/conemu/ConEmu/ConEmu.xml" "./windows/" "ConEmu config"
+cpask "$HOME/scoop/persist/conemu/ConEmu/ConEmu.xml" "./windows/" "ConEmu.xml"
 
 # Neovim
-cpask "$env:LOCALAPPDATA/nvim/init.vim" "./common/neovim" "Neovim config"
+cpask "$env:LOCALAPPDATA/nvim/init.vim" "./common/neovim" "init.vim"
 
 # Qutebrowser
-cpask "$env:APPDATA/qutebrowser/config/autoconfig.yml" "./common/qutebrowser/autoconfig.yml" "Qutebrowser config"
+cpask "$env:APPDATA/qutebrowser/config/autoconfig.yml" "./common/qutebrowser/autoconfig.yml" "Qutebrowser autoconfig"
 
 # Git
-cpask "~/.gitconfig" "./common/" "Git config"
+cpask "~/.gitconfig" "./common/" ".gitconfig"
 
 # Winfetch config
 cpask "~/.config/winfetch/config.ps1" "./windows/winfetch/config.ps1" "Winfetch config"
 
-Set-Location -
+
+git status
+If ($(Read-Host -Prompt "Stay in dotfiles? [Y/n]") -eq "n") {Set-Location -}
